ffd444539fdc927bc77abb07075ac0c8ec7629f9
Dev Kotecha <kotechas.dev@gmail.com>
2026-06-20 17:41:41 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,7 @@ int main(int argc, char *argv[])
         unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
         database_file.seekg(103); // skipping 100 bytes of header(since page1) and number of cells at offset 3
         char cntcells[2];
+        database_file.read(cntcells, 2);
         unsigned short cell_cnt = (static_cast<unsigned char>(cntcells[1]) | (static_cast<unsigned char>(cntcells[1]) << 8));
         cout << "database page size: " << page_size << endl;
         cout << "number of tables: " << cell_cnt << endl;
